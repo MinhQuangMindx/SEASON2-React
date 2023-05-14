d35be0ec89b5de66e67cb97f3e39ddf069aab4fe
MinhQuangMindx <vincentequang97@gmail.com>
2023-05-14 19:36:44 +0700

    Create main.js

---
+++ LESSION2/main.js
@@ -0,0 +1,46 @@
+import { encrypt_pass, quanghm2 } from "./helper"
+
+
+function login(username, password) {
+    const ePassword = encrypt_pass(password)
+}
+
+
+const arr1 = [1,2,3,4,5];
+function test(...args) {
+    args.forEach(Element =>{
+
+    });
+}
+
+test (1,2,3,4,5,6,7)
+
+let car = {
+    name: "honda",
+    price: 2000,
+    title: "sedan",
+    desc: "khuyen mai"
+}
+let red_car = {
+    ...car,
+    color: "red"
+}
+let blue_car = {
+    ...car,
+    color: "blue"
+}
+
+console.log(arr1);
+
+
+// 
+function test2(a) {
+    a.forEach(Element => {
+
+
+    });
+}
+
+test2([1,2,3,4,5,6,7])
+
+// Bài tập VN
